ba3ea2631fa0d2468824ff2224eda8752b52a409
Agustin <agustinmoncada94@gmail.com>
2026-03-25 20:33:26 -0300

    Forzando rutas de socios

--- api/index.js
+++ api/index.js
@@ -100,4 +100,5 @@ if (process.env.NODE_ENV !== 'production') {
     app.listen(PORT, () => console.log(`Servidor en http://localhost:${PORT}`));
 }
 
-module.exports = app; 
+module.exports = app;
+// Comentario para forzar subida
